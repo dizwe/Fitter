8cc089c4baa3661f0d874032deeb4a356ec5fa0e
dizwe <dizwe2716@gmail.com>
2017-08-07 15:08:23 +0900

    RESOLVE ANAL error because of the KEYWORD ARGUMENT.

--- fitterKakao/static/js/anal.js
+++ fitterKakao/static/js/anal.js
@@ -1,11 +1,11 @@
 function engToKor(param){
     var engKor= {'length':'길이','shoulder':'어깨','waist':'배','chest':'가슴','sleeve':'소매',
-           'bot_length':'기장','bot_waist':'허리','thigh':'허벅지','crotch':'밑위','hem':'밑단','hip':'엉덩이'};
+    'bot_length':'기장','bot_waist':'허리','thigh':'허벅지','crotch':'밑위','hem':'밑단','hip':'엉덩이'};
 
     return engKor[param]
-}
-
-function fitCal(calData,too_small_criteria=-30, big_criteria=20, too_criteria=50){
+};
+var too_small_criteria=-30; var big_criteria=20; var too_criteria=50;
+function fitCal(calData){
     var info = {'too_small': [],'small': [] ,'fit': [],'big': [],'too':[],'exception':[]};
     var excepts = ['길이','기장','소매','배','밑단'];
 
@@ -70,9 +70,11 @@ else if(clothes_type ==='bot'){
         delete botCal['hip'];
     }
     var divided_param =fitCal(botCal);
-}
+};
 
 $(document).ready(function(){
+    console.log('check!');
+
     var target = document.getElementsByClassName('anal')[0];
     target.innerHTML += "<p class ='anal-head'>[FIT=해당 부위 옷 길이-몸 길이]</p>";
     if(divided_param['too_small'].length>0){
@@ -80,35 +82,36 @@ $(document).ready(function(){
                             + "<span class ='anal-li block'>작은 부분</span>(FIT&lt;-3) : </span>"
                             + divided_param['too_small']+"</p>";
 
-    }
+    };
     if(divided_param['small'].length>0){
         target.innerHTML += "<p><span  class = 'anal-description'>"
                             + "<span class ='anal-li block'>옷이 안 늘어나면 작은 부분</span>(-3&lt;FIT&lt;0) : </span>"
                             + divided_param['small']+"</p>";
 
-    }
+    };
     if(divided_param['fit'].length>0){
 //        target.innerHTML += "<p class = 'anal-description'> 옷이 끼거나 딱 맞는 부분<span class ='block'>(0&lt;FIT&lt;2cm) :</span></p>";
 //        target.innerHTML += "<p>"+divided_param['fit']+"</p>";
         target.innerHTML += "<p><span  class = 'anal-description'>"
                             + "<span class ='anal-li block'>옷이 끼거나 딱 맞는 부분</span>(0&lt;FIT&lt;2cm) : </span>"
                             + divided_param['fit']+"</p>";
-    }
+    };
     if(divided_param['big'].length>0){
 //        target.innerHTML += "<p class = 'anal-description'> 옷이 넉넉한 부분<span class ='block'>(2&lt;FIT&lt;5cm) :</span></p>";
 //        target.innerHTML += "<p>"+divided_param['big']+"</p>";
         target.innerHTML += "<p><span  class = 'anal-description'>"
                             + "<span class ='anal-li block'>옷이 적당한 부분</span>(2&lt;FIT&lt;5cm) :  </span>"
                             + divided_param['big']+"</p>";
-    }
+    };
     if(divided_param['too'].length>0){
 //        target.innerHTML += "<p class = 'anal-description'> 옷이 큰 부분<span class ='block'>(5cm&lt;FIT) :</span></p>";
 //        target.innerHTML += "<p>"+divided_param['too']+"</p>";
         target.innerHTML += "<p><span  class = 'anal-description'>"
                             + "<span class ='anal-li    block'>옷이 큰 부분 </span>(5cm&lt;FIT) : </span>"
                             + divided_param['too']+"</p>";
-    }
+    };
     target.innerHTML += "<p class ='conclusion'><span class ='anal-li'>종합 추측하면, </span> "
                         +divided_param['conclusion']+"</p>";
+
 
 });
