fc33f459d92ce0809d957236fb73df7df96bb104
dizwe <dizwe2716@gmail.com>
2017-11-25 00:16:19 +0000

    Add admin list

--- fitterKakao/admin.py
+++ fitterKakao/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Person, TopClothes, BottomClothes, SizeInfo, SameClothes, SingleDataList
+from .models import Person, TopClothes, BottomClothes, SizeInfo, SameClothes, SingleDataList, CriteriaTop, CriteriaBottom
 # Register your models here.
 
 admin.site.register(Person)
@@ -9,5 +9,7 @@ admin.site.register(BottomClothes)
 admin.site.register(SizeInfo)
 admin.site.register(SameClothes)
 admin.site.register(SingleDataList)
+admin.site.register(CriteriaTop)
+admin.site.register(CriteriaBottom)
 
 # Register your models here.
